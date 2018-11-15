d361b0a8aafc63846f343e3419aa880dc4ae02b1
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation

--- calculator.h
+++ calculator.h
@@ -4,8 +4,19 @@
 class Calculator
 {
     public:
-        int Add (double, double);
-        int Sub (double, double);
+        int Add (int, int);
+        int Mul (double, double);
+        /*
+         * Lorem ipsum dorum... i forgot
+         *Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nisi massa, tempor eu aliquam non, aliquam ut dui. 
+         Donec vulputate sem eros, non iaculis diam sodales in. Nulla ornare metus eu justo tristique pellentesque. 
+         Duis sollicitudin ultrices tempus. Proin ultrices laoreet felis eu consequat. 
+         Donec convallis mauris eu eros ultricies commodo. Curabitur pellentesque arcu non mauris fringilla, a vulputate ex pellentesque. 
+         Nam ex quam, tempor at magna vel, cursus lobortis metus. Integer luctus rhoncus sollicitudin. 
+         Donec malesuada mauris leo, at pharetra lorem tempus id. Donec et nunc accumsan, sollicitudin sapien id, elementum tellus.
+         Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer in dui ut eros posuere varius. 
+         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+         * */
 };
 
 #endif//CALCULATOR_H
